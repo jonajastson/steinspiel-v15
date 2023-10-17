ba63cdcd962b5d4a7d137145aae162153cf1e644
jonajastson <147931240+jonajastson@users.noreply.github.com>
2023-10-17 19:46:15 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Spieler.change(LedSpriteProperty.X, -1)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    Spieler.set(LedSpriteProperty.X, -1)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Spieler.change(LedSpriteProperty.X, 1)
+    Spieler.ifOnEdgeBounce()
+})
+let Spieler: game.LedSprite = null
+let Steinhöhe = -5
+Spieler = game.createSprite(2, 4)
+let Stein = game.createSprite(randint(0, 4), Steinhöhe)
+basic.forever(function () {
+    Steinhöhe += 1
+    if (Steinhöhe < 0) {
+        Stein.set(LedSpriteProperty.Brightness, 10)
+    } else {
+        Stein.set(LedSpriteProperty.Brightness, 255)
+    }
+    Stein.set(LedSpriteProperty.Y, Steinhöhe)
+    basic.pause(100)
+})
